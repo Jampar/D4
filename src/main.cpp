847a97bb3412d23570665d1eb73da83b989e4509
J.Parkes <james@ancroft.eu>
2023-03-04 13:41:18 +0000

    Torch and Motor Operation

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,85 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-#include "Arduino.h"
+#include<Arduino.h>
+#include <Stepper.h>
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 13
+#pragma region (Stepper Motor Coils)
+    #define COIL_1 8
+    #define COIL_2 9
+    #define COIL_3 10
+    #define COIL_4 11
+#pragma endregion
 
-#define BLINK_RATE 500
+#pragma region (Gearing/Step Vars)
+    #define STEP_ANGLE 0.17578125
+#pragma endregion
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+#pragma region (Stepper Control Vars)
+    const int stepPerRotation = 360/STEP_ANGLE;
+    Stepper stepperControl = Stepper(stepPerRotation, COIL_1, COIL_3, COIL_2, COIL_4);
+    int stepperSpeed = 10;
+    int deltaSteps = 0;
+#pragma endregion
+
+#define TORCH_PIN 2
+
+void StepMotor(int steps){
+    //Move stepper
+    stepperControl.step(steps);
+    //Track global position
+    deltaSteps += steps;
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+void SetMotorPosition(int degrees){
 
-  // wait for a second
-  delay(BLINK_RATE);
+    //Calculate steps required for angle
+    int stepCount = degrees/STEP_ANGLE;
+    //Apply global position
+    stepCount -= deltaSteps;
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+    //Output instruction
+    Serial.print("Position: ");
+    Serial.print(degrees);
+    Serial.print(" Steps: ");
+    Serial.print(stepCount);
+    Serial.println();
 
-   // wait for a second
-  delay(BLINK_RATE);
+    //Step the motor
+    StepMotor(stepCount);
+}
+
+//Return to starting position
+void MotorReturn(){
+
+    Serial.print("Returning...");
+    Serial.print(deltaSteps);
+    Serial.println("steps.");
+
+    StepMotor(-deltaSteps);
+}
+
+void SetTorchStatus(PinStatus status){
+    digitalWrite(TORCH_PIN, status);
+}
+
+void setup() {
+    //Serial Comms Setup
+    Serial.begin(9600);
+    Serial.println("Starting...");
+    
+    //Assign Torch Pin
+    pinMode(TORCH_PIN, OUTPUT);
+
+    //Set Stepper Speed
+    stepperControl.setSpeed(stepperSpeed);
+}
+
+void loop() {
+    SetMotorPosition(360);
+    delay(1000);
+    MotorReturn();
+    delay(1000);
+
+    SetTorchStatus(HIGH);
+    delay(1000);
+    SetTorchStatus(LOW);
+    delay(1000);
 }
