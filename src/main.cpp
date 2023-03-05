aefce1bf48d5f25302e1171cc7258fe0e8b1e3b6
J.Parkes <james@ancroft.eu>
2023-03-05 17:15:16 +0000

    Added Laser + ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,124 @@
-#include<Arduino.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <vl53lx_class.h>
 #include <Stepper.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdlib.h>
 
-#pragma region (Stepper Motor Coils)
-    #define COIL_1 8
-    #define COIL_2 9
-    #define COIL_3 10
-    #define COIL_4 11
-#pragma endregion
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13
+#endif
 
-#pragma region (Gearing/Step Vars)
-    #define STEP_ANGLE 0.17578125
-#pragma endregion
-
-#pragma region (Stepper Control Vars)
-    const int stepPerRotation = 360/STEP_ANGLE;
-    Stepper stepperControl = Stepper(stepPerRotation, COIL_1, COIL_3, COIL_2, COIL_4);
-    int stepperSpeed = 10;
-    int deltaSteps = 0;
-#pragma endregion
-
+#define LedPin LED_BUILTIN
 #define TORCH_PIN 2
 
-void StepMotor(int steps){
-    //Move stepper
-    stepperControl.step(steps);
-    //Track global position
-    deltaSteps += steps;
+#pragma region (Stepper Motor Coils)
+   #define COIL_1 8
+   #define COIL_2 9
+   #define COIL_3 10
+   #define COIL_4 11
+
+   #define STEP_ANGLE 0.17578125
+
+   const int stepPerRotation = 360/STEP_ANGLE;
+   Stepper stepperControl = Stepper(stepPerRotation, COIL_1, COIL_3, COIL_2, COIL_4);
+   int stepperSpeed = 10;
+   int deltaSteps = 0;
+#pragma endregion
+
+
+VL53LX sensor_vl53lx_sat(&Wire, 25); // Sensor Addressing
+int16_t prevDist = 0;
+int distChangeThreshold = 20;
+
+void SetupDistanceFinder(){
+   //---Init Sensor procedure---
+   sensor_vl53lx_sat.begin();
+   sensor_vl53lx_sat.VL53LX_Off();
+   sensor_vl53lx_sat.InitSensor(0x12);
+   sensor_vl53lx_sat.VL53LX_StartMeasurement();
 }
 
-void SetMotorPosition(int degrees){
+int16_t GetDistance(){
 
-    //Calculate steps required for angle
-    int stepCount = degrees/STEP_ANGLE;
-    //Apply global position
-    stepCount -= deltaSteps;
+   //Ranging Data variable & pointer
+   VL53LX_MultiRangingData_t MultiRangingData;
+   VL53LX_MultiRangingData_t *pMultiRangingData = &MultiRangingData;
 
-    //Output instruction
-    Serial.print("Position: ");
-    Serial.print(degrees);
-    Serial.print(" Steps: ");
-    Serial.print(stepCount);
-    Serial.println();
+   //Data flag
+   uint8_t NewDataReady = 0;
 
-    //Step the motor
-    StepMotor(stepCount);
+   //Multiple object count
+   int no_of_object_found = 0, j;
+   //Return status
+   int status;
+
+   //Retrieve distance data when available
+   do
+   {
+      status = sensor_vl53lx_sat.VL53LX_GetMeasurementDataReady(&NewDataReady);
+   } while (!NewDataReady);
+
+   //Indicator LED
+   digitalWrite(LedPin, HIGH);
+
+   //Retrieve Data, if status error - reset interrupts
+   if((!status)&&(NewDataReady!=0))
+   {
+      status = sensor_vl53lx_sat.VL53LX_GetMultiRangingData(pMultiRangingData);
+      if (status==0)
+      {
+         status = sensor_vl53lx_sat.VL53LX_ClearInterruptAndStartMeasurement();
+      }
+   }
+
+   //Disable Inidicator
+   digitalWrite(LedPin, LOW);
+
+   return pMultiRangingData->RangeData[j].RangeMilliMeter;
 }
 
-//Return to starting position
-void MotorReturn(){
-
-    Serial.print("Returning...");
-    Serial.print(deltaSteps);
-    Serial.println("steps.");
-
-    StepMotor(-deltaSteps);
+int16_t GetMeanDistance(){
+   int sampleCount = 1;
+   int total = 0;
+   for(int i = 0; i < sampleCount; i++){
+      int dist = GetDistance();
+      if(dist > 3000) i--;
+      else total += GetDistance();
+   }
+   int16_t averageDist = total/sampleCount;
+   if(abs(averageDist - prevDist) > distChangeThreshold){
+      prevDist = averageDist;
+      return averageDist;
+   }else{
+      return prevDist;
+   }
 }
 
-void SetTorchStatus(PinStatus status){
-    digitalWrite(TORCH_PIN, status);
+void setup()
+{
+   // Led.
+   pinMode(LedPin, OUTPUT);
+   pinMode(TORCH_PIN, OUTPUT);
+
+   // Initialize serial for output.
+   Serial.begin(9600);
+   Serial.println("Starting...");
+
+   // Initialize I2C bus.
+   Wire.begin();
+
+   SetupDistanceFinder();
 }
 
-void setup() {
-    //Serial Comms Setup
-    Serial.begin(9600);
-    Serial.println("Starting...");
-    
-    //Assign Torch Pin
-    pinMode(TORCH_PIN, OUTPUT);
+void loop()
+{
+   int16_t dist = GetMeanDistance();
 
-    //Set Stepper Speed
-    stepperControl.setSpeed(stepperSpeed);
-}
-
-void loop() {
-    SetMotorPosition(360);
-    delay(1000);
-    MotorReturn();
-    delay(1000);
-
-    SetTorchStatus(HIGH);
-    delay(1000);
-    SetTorchStatus(LOW);
-    delay(1000);
+   if(dist < 100) digitalWrite(TORCH_PIN,HIGH);
+   else digitalWrite(TORCH_PIN,LOW);
 }
